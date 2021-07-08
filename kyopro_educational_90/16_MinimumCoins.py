a8c98b6836a837b0aea2deaf02189a8271105cf0
hrkhrkhrk <87046753+hrkhrkhrk@users.noreply.github.com>
2021-07-08 23:35:00 +0900

    Add files via upload

---
+++ kyopro_educational_90/16_MinimumCoins.py
@@ -0,0 +1,16 @@
+N = int(input())
+X = list(map(int, input().split()))
+X = sorted(X, reverse=True)
+A = X[0]
+B = X[1]
+C = X[2]
+for n in range(N//A, N//C+1):
+    for x in range(n+1):
+        p = N-C*n-(A-C)*x
+        if p%(B-C) == 0:
+            y = p//(B-C)
+            z = n-y-x
+            if 0 <= y <= n and 0 <= z <= n:
+                n = x + y + z
+                print(n)
+                exit()
